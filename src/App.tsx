ce29ddbb1184251ada8cb6bacf5ae38f3189aebe
Atef Ben Ali <atef.bettaib@gmail.com>
2021-05-09 15:34:12 +0100

    style: format

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
-import './App.css';
+import "./App.css";
 
-import React from 'react';
+import React from "react";
 
 export default function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p className="header">Vite + React + Typescript</p>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <p className="header">Vite + React + Typescript</p>
+            </header>
+        </div>
+    );
 }
