115d14c15b19897f0bdc72d1d7156345ca415da1
Atef Ben Ali <atef.bettaib@gmail.com>
2021-05-09 15:29:46 +0100

    init commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css';
+
+import React from 'react';
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p className="header">Vite + React + Typescript</p>
+      </header>
+    </div>
+  );
+}
